dc7e6e6a23759240e6747c4fd9e82e9a50d16983
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-04-16 19:21:01 +0300

    Initial version of Apollo Cache Router

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# apollo-cache-router
-Apollo Cache Router - GraphQL cache router, filter and logger for Apollo Cache
+## Apollo Cache Router
+
+[![Twitter Follow](https://img.shields.io/twitter/follow/sysgears.svg?style=social)](https://twitter.com/sysgears)
+
+## Installation
+
+```bash
+npm install --save-dev apollo-cache-router
+```
+
+## Usage
+``` js
+const netCache = new InMemoryCache();
+const localCache = new InMemoryCache();
+const cache = ApolloCacheRouter.override(
+  ApolloCacheRouter.route([netCache, localCache], document => {
+    if (hasDirectives(['client'], document) || getOperationAST(document).name.value === 'GeneratedClientQuery') {
+      // Pass all @client queries and @client defaults to localCache
+      return [localCache];
+    } else {
+      // Pass all the other queries to netCache
+      return [netCache];
+    }
+  }),
+  {
+    reset: () => {
+      // On apolloClient.resetStore() reset only netCache and keep localCache intact
+      return netCache.reset();
+    }
+  }
+);
+```
+
+## License
+Copyright © 2018 [SysGears LTD]. This source code is licensed under the [MIT] license.
+
+[MIT]: LICENSE
+[SysGears LTD]: http://sysgears.com
---
+++ src/index.ts
@@ -0,0 +1,270 @@
+import { ApolloCache, Cache, Transaction } from 'apollo-cache';
+import { DocumentNode } from 'graphql';
+
+export interface OverrideOptions<TCache> {
+  read?: <T>(query: Cache.ReadOptions) => T;
+  write?: (write: Cache.WriteOptions) => void;
+  diff?: <T>(query: Cache.DiffOptions) => Cache.DiffResult<T>;
+  watch?: (watch: Cache.WatchOptions) => () => void;
+  evict?: (query: Cache.EvictOptions) => Cache.EvictionResult;
+  reset?: () => Promise<void>;
+  restore?: (serializedState: TCache) => ApolloCache<TCache>;
+  extract?: (optimistic: boolean) => TCache;
+  removeOptimistic?: (id: string) => void;
+  performTransaction?: (transaction: Transaction<any>) => void;
+  recordOptimisticTransaction?: (transaction: Transaction<any>, id: string) => void;
+  transformDocument?: (document: DocumentNode) => DocumentNode;
+}
+
+class OverrideCache<TCache> extends ApolloCache<TCache> {
+  private cache: ApolloCache<TCache>;
+  private options: OverrideOptions<TCache>;
+
+  constructor(cache: ApolloCache<TCache>, options?: OverrideOptions<TCache>) {
+    super();
+    this.cache = cache;
+    this.options = options || {};
+  }
+
+  public read<T>(query: Cache.ReadOptions): T {
+    return this.options.read ? this.options.read(query) : this.cache.read(query);
+  }
+
+  public write(write: Cache.WriteOptions): void {
+    this.options.write ? this.options.write(write) : this.cache.write(write);
+  }
+
+  public diff<T>(query: Cache.DiffOptions): Cache.DiffResult<T> {
+    return this.options.diff ? this.options.diff(query) : this.cache.diff(query);
+  }
+
+  public watch(watch: Cache.WatchOptions): () => void {
+    return this.options.watch ? this.options.watch(watch) : this.cache.watch(watch);
+  }
+
+  public evict(query: Cache.EvictOptions): Cache.EvictionResult {
+    return this.options.evict ? this.options.evict(query) : this.cache.evict(query);
+  }
+
+  public reset(): Promise<void> {
+    return this.options.reset ? this.options.reset() : this.cache.reset();
+  }
+
+  public restore(serializedState: TCache): ApolloCache<TCache> {
+    return this.options.restore ? this.options.restore(serializedState) : this.cache.restore(serializedState);
+  }
+
+  public extract(optimistic: boolean): TCache {
+    return this.options.extract ? this.options.extract(optimistic) : this.cache.extract(optimistic);
+  }
+
+  public removeOptimistic(id: string): void {
+    this.options.removeOptimistic ? this.options.removeOptimistic(id) : this.cache.removeOptimistic(id);
+  }
+
+  public performTransaction(transaction: Transaction<any>): void {
+    this.options.performTransaction
+      ? this.options.performTransaction(transaction)
+      : this.cache.performTransaction(transaction);
+  }
+
+  public recordOptimisticTransaction(transaction: Transaction<any>, id: string): void {
+    this.options.recordOptimisticTransaction
+      ? this.options.recordOptimisticTransaction(transaction, id)
+      : this.cache.recordOptimisticTransaction(transaction, id);
+  }
+
+  public transformDocument(document: DocumentNode): DocumentNode {
+    return this.options.transformDocument
+      ? this.options.transformDocument(document)
+      : this.cache.transformDocument(document);
+  }
+}
+
+export const override = <TCache>(cache: ApolloCache<TCache>, options?: OverrideOptions<TCache>): ApolloCache<TCache> =>
+  new OverrideCache<TCache>(cache, options);
+
+export type RouterFunction = (document: DocumentNode, caches: Array<ApolloCache<any>>) => ApolloCache<any>;
+
+class RoutingCache extends ApolloCache<any> {
+  private caches: Array<ApolloCache<any>>;
+  private router: RouterFunction;
+
+  constructor(caches: Array<ApolloCache<any>>, router: RouterFunction) {
+    super();
+    this.caches = caches;
+    this.router = router;
+  }
+
+  public read<T>(query: Cache.ReadOptions): T {
+    const caches = this.check(this.router(query.query, this.caches));
+    if (caches.length === 0) {
+      throw new Error('No caches to read from for the document');
+    } else {
+      return caches[0].read(query);
+    }
+  }
+
+  public write(write: Cache.WriteOptions): void {
+    return this.check(this.router(write.query, this.caches)).forEach(cache => cache.write(write));
+  }
+
+  public diff<T>(query: Cache.DiffOptions): Cache.DiffResult<T> {
+    const caches = this.check(this.router(query.query, this.caches));
+    return caches.length === 0 ? { result: null as T, complete: false } : caches[0].diff(query);
+  }
+
+  public watch(watch: Cache.WatchOptions): () => void {
+    const caches = this.check(this.router(watch.query, this.caches));
+
+    return caches.length === 0 ? () => {} : caches[0].watch(watch);
+  }
+
+  public evict(query: Cache.EvictOptions): Cache.EvictionResult {
+    let result;
+    this.check(this.router(query.query, this.caches)).forEach(cache => (result = cache.evict(query)));
+    return result;
+  }
+
+  public reset(): Promise<any> {
+    return Promise.all(this.caches.map(cache => cache.reset()));
+  }
+
+  public extract(optimistic: boolean): any {
+    const state = {};
+    for (let idx = 0; idx < this.caches.length; idx++) {
+      state['cache' + idx] = this.caches[idx].extract(optimistic);
+    }
+    return state;
+  }
+
+  public restore(serializedState: any): ApolloCache<any> {
+    for (let idx = 0; idx < this.caches.length; idx++) {
+      this.caches[idx].restore(serializedState['cache' + idx]);
+    }
+    return this;
+  }
+
+  public removeOptimistic(id: string): void {
+    this.caches.forEach(cache => cache.removeOptimistic(id));
+  }
+
+  public performTransaction(transaction: Transaction<any>): void {
+    this.caches.forEach(cache => cache.performTransaction(transaction));
+  }
+
+  public recordOptimisticTransaction(transaction: Transaction<any>, id: string): void {
+    this.caches.forEach(cache => cache.recordOptimisticTransaction(transaction, id));
+  }
+
+  public transformDocument(document: DocumentNode): DocumentNode {
+    return this.caches[0].transformDocument(document);
+  }
+
+  private check(caches: ApolloCache<any> | Array<ApolloCache<any>>): Array<ApolloCache<any>> {
+    const cacheList = !caches
+      ? []
+      : caches.constructor === Array
+        ? (caches as Array<ApolloCache<any>>)
+        : ([caches] as Array<ApolloCache<any>>);
+    if (process.env.NODE_ENV !== 'production') {
+      for (const cache of cacheList) {
+        if (this.caches.indexOf(cache) < 0) {
+          throw new Error('Router must return a subset from submitted caches');
+        }
+      }
+    }
+    return cacheList;
+  }
+}
+
+export const route = (caches: Array<ApolloCache<any>>, router: RouterFunction): ApolloCache<any> =>
+  new RoutingCache(caches, router);
+
+export interface LogOptions {
+  logger?: (msg: string) => void;
+  formatter?: (methodName: string, args: any[], result: any) => string;
+}
+
+class LogCache<TCache> extends ApolloCache<TCache> {
+  private cache: ApolloCache<TCache>;
+  private options: LogOptions;
+
+  constructor(cache: ApolloCache<TCache>, options?: LogOptions) {
+    super();
+    this.cache = cache;
+    this.options = options || {};
+  }
+
+  public read<T>(query: Cache.ReadOptions): T {
+    return this.log('read', [query], this.cache.read(query));
+  }
+
+  public write(write: Cache.WriteOptions): void {
+    return this.log('write', [write], this.cache.write(write));
+  }
+
+  public diff<T>(query: Cache.DiffOptions): Cache.DiffResult<T> {
+    return this.log('diff', [query], this.cache.diff(query));
+  }
+
+  public watch(watch: Cache.WatchOptions): () => void {
+    return this.log('watch', [watch], this.cache.watch(watch));
+  }
+
+  public evict(query: Cache.EvictOptions): Cache.EvictionResult {
+    return this.log('evict', [query], this.cache.evict(query));
+  }
+
+  public reset(): Promise<void> {
+    this.log('reset', [], undefined);
+    return this.cache.reset();
+  }
+
+  public restore(serializedState: TCache): ApolloCache<TCache> {
+    return this.log('restore', [serializedState], this.cache.restore(serializedState));
+  }
+
+  public extract(optimistic: boolean): TCache {
+    return this.log('extract', [optimistic], this.cache.extract(optimistic));
+  }
+
+  public removeOptimistic(id: string): void {
+    this.log('removeOptimistic', [id], undefined);
+    this.cache.removeOptimistic(id);
+  }
+
+  public performTransaction(transaction: Transaction<any>): void {
+    this.log('performTransaction', [transaction], 'start');
+    this.cache.performTransaction(transaction);
+    this.log('performTransaction', [transaction], 'end');
+  }
+
+  public recordOptimisticTransaction(transaction: Transaction<any>, id: string): void {
+    this.log('recordOptimisticTransaction', [transaction, id], 'start');
+    this.cache.recordOptimisticTransaction(transaction, id);
+    this.log('recordOptimisticTransaction', [transaction, id], 'end');
+  }
+
+  public transformDocument(document: DocumentNode): DocumentNode {
+    return this.log('transformDocument', [document], this.cache.transformDocument(document));
+  }
+
+  private log<T>(methodName: string, args: any[], result: T): T {
+    const msg =
+      this.options && this.options.formatter
+        ? this.options.formatter(methodName, args, result)
+        : methodName + JSON.stringify(args) + ' -> ' + JSON.stringify(result);
+    ((this.options && this.options.logger) || console.log)(msg);
+    return result;
+  }
+}
+
+export const log = <TCache>(cache: ApolloCache<TCache>, options?: LogOptions): ApolloCache<TCache> =>
+  new LogCache(cache, options);
+
+export default class ApolloCacheLink {
+  public static override = override;
+  public static route = route;
+  public static log = log;
+}
